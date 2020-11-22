b60d426dea9c9dc0d015432b83e3387b6620ce61
Nestorsoiza <nestor.soiza83@gmail.com>
2020-11-22 19:37:54 +0100

    Añado clase abstracta vehiculo con ruedas

---
+++ src/vehiculos/VehiculosConRuedas.java
@@ -0,0 +1,19 @@
+package vehiculos;
+
+public abstract class VehiculosConRuedas extends Vehiculo{
+	public abstract int getNumeroDeRuedas();
+	
+	public VehiculosConRuedas() {
+		super();
+	}
+
+	public VehiculosConRuedas(String modelo, String color) {
+		super(modelo, color);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", " + getNumeroDeRuedas() + " ruedas ";
+	}
+
+}
